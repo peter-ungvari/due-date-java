9c01d7a9c813fea1b02bd2f43ffd8762facfb417
Peter Ungvari <ungvari.mail@gmail.com>
2016-09-10 14:29:34 +0200

    validation and some more edge test cases added

--- src/main/java/org/example/duedate/DueDateCalculator.java
+++ src/main/java/org/example/duedate/DueDateCalculator.java
@@ -1,18 +1,26 @@
 package org.example.duedate;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class DueDateCalculator {
+
+    private static final int FIVE_PM = 17;
+    private static final int NINE_AM = 9;
 
     public Date calculateDueDate(Date submitDate, int turnAroundHours) {
 	Calendar calendar = Calendar.getInstance();
 	calendar.setTime(submitDate);
-	
-	for(int i = 0; i < turnAroundHours; ++i) {
+
+	validateSubmitDate(calendar);
+	validateTurnAroundHours(turnAroundHours);
+
+	for (int i = 0; i < turnAroundHours; ++i) {
 	    nextWorkHour(calendar);
 	}
-	
+
 	return calendar.getTime();
     }
 
@@ -24,7 +32,7 @@ public class DueDateCalculator {
 	    calendar.add(Calendar.HOUR_OF_DAY, 1);
 	}
     }
-    
+
     private void nextWeekDay(Calendar calendar) {
 	if (Calendar.FRIDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
 	    calendar.add(Calendar.DATE, 3); // MONDAY
@@ -32,5 +40,27 @@ public class DueDateCalculator {
 	    calendar.add(Calendar.DATE, 1); // NEXT calendar day
 	}
     }
-    
+
+    private void validateSubmitDate(Calendar calendar) {
+	List<Integer> weekends = Arrays.asList(Calendar.SUNDAY, Calendar.SATURDAY);
+
+	if (weekends.contains(calendar.get(Calendar.DAY_OF_WEEK))) {
+	    throw new IllegalArgumentException("Submit date is not a weekday. Only weekdays accepted");
+	}
+
+	if (calendar.get(Calendar.HOUR_OF_DAY) < NINE_AM) {
+	    throw new IllegalArgumentException("Submit time is before 9AM. It should be between 9AM and 5PM");
+	}
+
+	if (calendar.get(Calendar.HOUR_OF_DAY) > FIVE_PM) {
+	    throw new IllegalArgumentException("Submit time is after 5PM. It should be between 9AM and 5PM");
+	}
+    }
+
+    private void validateTurnAroundHours(int turnAroundHours) {
+	if (turnAroundHours <= 0) {
+	    throw new IllegalArgumentException("turnAroundHours should be a positive integer");
+	}
+    }
+
 }
--- src/test/java/org/example/duedate/DueDateCalculatorTest.java
+++ src/test/java/org/example/duedate/DueDateCalculatorTest.java
@@ -8,66 +8,117 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DueDateCalculatorTest {
-    
+
     private static final int MAX_RANDOM_TURN_AROUND_HOURS = 500;
     private static final long RANDOM_SEED = 21L;
-    
+    private static final int NUM_RANDOM_ITERATIONS = 100;
+
     private DueDateCalculator dueDateCalculator = new DueDateCalculator();
     private Random random = new Random(RANDOM_SEED);
-    
-    @Test
-    public void testCaculateDueDateAfterStartTime() {
-	Date startDate = new Date(System.currentTimeMillis() + random.nextLong());
-	int turnAroundHours = random.nextInt(MAX_RANDOM_TURN_AROUND_HOURS) + 1; //greater than 0
-	
-	Date dueDate = dueDateCalculator.calculateDueDate(startDate, turnAroundHours);
-	
-	assertTrue("dueDate is greater than startDate", dueDate.after(startDate));
-    }
-    
+
     @Test
     public void testCaculateDueDateFrom9AMto5PM() {
-	Date startDate = new Date(System.currentTimeMillis() + random.nextLong());
-	int turnAroundHours = random.nextInt(MAX_RANDOM_TURN_AROUND_HOURS) + 1; //greater than 0
-	
-	Date dueDate = dueDateCalculator.calculateDueDate(startDate, turnAroundHours);
-	
-	Calendar calendar = Calendar.getInstance();
-	calendar.setTime(dueDate);
-	
-	assertTrue("from 9AM", calendar.get(Calendar.HOUR_OF_DAY) >= 9);
-	assertTrue("to 5PM", calendar.get(Calendar.HOUR_OF_DAY) <= 17);
+	for (int i = 0; i < NUM_RANDOM_ITERATIONS; ++i) {
+	    Calendar submitCalendar = Calendar.getInstance();
+	    submitCalendar.set(Calendar.YEAR, random.nextInt(2000) + 1000);
+	    submitCalendar.set(Calendar.MONTH, random.nextInt(12));
+	    submitCalendar.set(Calendar.HOUR_OF_DAY, random.nextInt(8) + 9);
+	    submitCalendar.set(Calendar.DAY_OF_WEEK, random.nextInt(5) + 2);
+
+	    int turnAroundHours = random.nextInt(MAX_RANDOM_TURN_AROUND_HOURS) + 1; //positive
+
+	    Date dueDate = dueDateCalculator.calculateDueDate(submitCalendar.getTime(), turnAroundHours);
+
+	    Calendar calendar = Calendar.getInstance();
+	    calendar.setTime(dueDate);
+
+	    assertTrue("dueDate is greater than startDate", dueDate.after(submitCalendar.getTime()));
+	    assertTrue("from 9AM ", calendar.get(Calendar.HOUR_OF_DAY) >= 9);
+	    assertTrue("to 5PM", calendar.get(Calendar.HOUR_OF_DAY) <= 17);
+	}
     }
-    
+
     @Test
     public void testCaculateDueDate1DayOverflow() {
 	Calendar submitCalendar = new GregorianCalendar(2016, 8, 7, 14, 0); // 2016-09-07 2PM
 
 	int turnAroundHours = 6;
-	
+
 	Date dueDate = dueDateCalculator.calculateDueDate(submitCalendar.getTime(), turnAroundHours);
-	
+
 	Calendar calendar = new GregorianCalendar(2016, 8, 8, 12, 0); // 2016-09-07 12PM
 	calendar.setTime(dueDate);
 	assertEquals("2016-09-07 2PM + 6 hours = 2016-09-07 12PM", calendar.getTime(), dueDate);
     }
-    
+
     @Test
     public void testCaculateDueDateNear5PM() {
 	Calendar submitCalendar = Calendar.getInstance();
 	submitCalendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
 	submitCalendar.set(Calendar.HOUR_OF_DAY, 16);
 	submitCalendar.set(Calendar.MINUTE, 1);
-	
+
 	Date startDate = submitCalendar.getTime();
 	int turnAroundHours = 1;
-	
+
 	Date dueDate = dueDateCalculator.calculateDueDate(startDate, turnAroundHours);
-	
+
 	Calendar calendar = Calendar.getInstance();
 	calendar.setTime(dueDate);
-	
+
 	assertEquals("9AM is used consistently isntead of 5PM", 9, calendar.get(Calendar.HOUR_OF_DAY));
     }
-    
+
+    @Test
+    public void testCaculateDueDateMoreDaysOverflow() {
+	Calendar submitCalendar = new GregorianCalendar(2016, 8, 9, 14, 0); // 2016-09-09 2PM
+
+	int turnAroundHours = 24 * 7 + 4; //7 days, 4 hours
+
+	Date dueDate = dueDateCalculator.calculateDueDate(submitCalendar.getTime(), turnAroundHours);
+
+	Calendar calendar = new GregorianCalendar(2016, 8, 19, 10, 0); // 2016-09-19 10AM
+	calendar.setTime(dueDate);
+	assertEquals("2016-09-09 2PM + 7 days 4 hours = 2016-09-19 10AM", calendar.getTime(), dueDate);
+    }
+
+    @Test
+    public void testCaculateDueDateYearOverflow() {
+	Calendar submitCalendar = new GregorianCalendar(2015, 11, 31, 14, 0); // 2015-12-31 2PM
+
+	int turnAroundHours = 4;
+
+	Date dueDate = dueDateCalculator.calculateDueDate(submitCalendar.getTime(), turnAroundHours);
+
+	Calendar calendar = new GregorianCalendar(2016, 0, 1, 10, 0); // 2016-01-01 10AM
+	calendar.setTime(dueDate);
+	assertEquals("2015-12-31 2PM + 4 hours = 2016-01-01 10AM", calendar.getTime(), dueDate);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCaculateDueDateValidateSubmitHours() {
+	Calendar submitCalendar = Calendar.getInstance();
+	submitCalendar.set(Calendar.HOUR_OF_DAY, 0); //midnight
+	dueDateCalculator.calculateDueDate(submitCalendar.getTime(), 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCaculateDueDateValidateTurnAroundHours() {
+	Calendar submitCalendar = new GregorianCalendar(2016, 8, 7, 14, 0); // 2016-09-07 12PM
+	dueDateCalculator.calculateDueDate(submitCalendar.getTime(), 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCaculateDueDateValidateTurnAroundHours2() {
+	Calendar submitCalendar = new GregorianCalendar(2016, 8, 7, 14, 0); // 2016-09-07 12PM
+	dueDateCalculator.calculateDueDate(submitCalendar.getTime(), -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCaculateDueDateValidateSubmitWeekdays() {
+	Calendar submitCalendar = Calendar.getInstance();
+	submitCalendar.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
+	dueDateCalculator.calculateDueDate(submitCalendar.getTime(), 1);
+    }
+
 }
