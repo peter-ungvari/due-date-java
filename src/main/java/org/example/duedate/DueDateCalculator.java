ba546f4250379680e2f2255925837d1eaf316559
Peter Ungvari <ungvari.mail@gmail.com>
2016-09-10 12:37:07 +0200

    init

---
+++ src/main/java/org/example/duedate/DueDateCalculator.java
@@ -0,0 +1,10 @@
+package org.example.duedate;
+
+import java.util.Date;
+
+public class DueDateCalculator {
+    
+    public Date calculateDueDate(Date submitDate, int turnAroundHours) {
+	return null;
+    }
+}
